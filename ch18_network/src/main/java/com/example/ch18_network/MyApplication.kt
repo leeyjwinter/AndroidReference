4a5e12a6541e69550e0d399f443baa121b2ea36d
leeyjwinter <james_22@naver.com>
2022-10-11 16:02:38 +0900

    Initial commit

---
+++ ch18_network/src/main/java/com/example/ch18_network/MyApplication.kt
@@ -0,0 +1,29 @@
+package com.example.ch18_network
+
+import android.app.Application
+import com.example.ch18_network.retrofit.NetworkService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MyApplication: Application() {
+
+    companion object {
+        val QUERY = "travel"
+        val API_KEY = "fcbb5ff9fe7847b190b3ab53fd4fe6df"
+        val BASE_URL = "https://newsapi.org"
+        val USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
+
+
+        //add....................................
+        var networkService: NetworkService
+        val retrofit: Retrofit
+            get() = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        init {
+            networkService = retrofit.create(NetworkService::class.java)
+        }
+    }
+
+}
